0da998ee2fd4c04434428279e60adf5c0fd924c4
Ryan Beck <ryanbeck@Ryans-MBP.fios-router.home>
2016-08-14 15:15:59 -0700

    overlay change

--- js/vegas-info.js
+++ js/vegas-info.js
@@ -13,11 +13,14 @@ $(window).load(function() {
     setTimeout(function() {
 
         $('body').vegas({
+            /*
             slides: [
                 { src: 'img/slide-1.jpg' },
                 { src: 'img/slide-2.jpg' },
                 { src: 'img/slide-3.jpg' },
             ],
+            */
+            slides :[],
 
             // Delay beetween slides in milliseconds.
             delay: 5000,
