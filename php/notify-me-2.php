e4f0d841e3ebfe681a33d6930b9f42b9d2cb3a85
Ryan Beck <ryanbeck@Ryans-MBP.fios-router.home>
2016-08-14 23:28:02 -0700

    emails

--- php/notify-me-2.php
+++ php/notify-me-2.php
@@ -6,9 +6,9 @@ header('content-type: application/json');
 	$o->status = 'success';
 	echo json_encode($o);
 
-	$email_to = "jb08090@gmail.com"; // Replace by your email address
+	$email_to = "steve@pray.com, ryan@bextechnologies.com"; // Replace by your email address
 	$email = $_POST["email"];
-	$text = "Congratulations ! A new person wants to be alerted when your site will be online: $email";
+	$text = "Congratulations! A new person wants to be alerted when your site will be online: $email";
 
 	$headers = "MIME-Version: 1.0" . "\r\n"; 
 	$headers .= "Content-type:text/html; charset=utf-8" . "\r\n"; 
