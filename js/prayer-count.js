93cedeb9b14c2819b1877c6db5ae7f60de5dbe93
Ryan Beck <ryanbeck@Ryans-MBP.fios-router.home>
2016-08-28 13:15:05 -0700

    italian flag

--- js/prayer-count.js
+++ js/prayer-count.js
@@ -29,7 +29,7 @@
           'Content-Type' : 'application/json'
         },
         url: "https://wsmtet5y0c.execute-api.us-east-1.amazonaws.com/prod/prayer-count",
-        data: 'olympic-games'
+        data: 'italy-earthquake'
       }).done(function( response ) {
           console.log( response );
           document.getElementById("result").innerHTML = "";
