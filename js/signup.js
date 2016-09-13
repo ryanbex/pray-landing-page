f197a70fb1e2d5ff67e53af9b6573db8f9a0e3d2
Ryan Beck <ryan@bextechnologies.com>
2016-09-12 22:32:46 -0700

    pray email subscribe added

--- js/signup.js
+++ js/signup.js
@@ -1,7 +1,25 @@
 
-$(".signup").click(
-	ga('pray.send', 'event', 'buttons', 'click', 'subscribe', 1);
-	ga('bex.send', 'event', 'buttons', 'click', 'subscribe', 1);
-);
+$("#maitre-submit-button").click( function() {
+	ga('pray.send', 'event', 'buttons', 'click', 'signup', 1);
+	ga('bex.send', 'event', 'buttons', 'click', 'signup', 1);
+	var email = $("input[type=email]").val();
+	if (email) {
+		subscribe(email);
+	}
+});
 
-        
+function subscribe(email) {
+	$.ajax({
+		method: "PUT",
+		headers: {
+		  'Content-Type' : 'application/json'
+		},
+		url: "https://l4dn8puutj.execute-api.us-east-1.amazonaws.com/prod/api/v1/email",
+		data: email
+	}).done(function( response ) {
+		console.log( response );
+		ga('pray.send', 'event', 'buttons', 'click', 'signup-success', 1);
+		ga('bex.send', 'event', 'buttons', 'click', 'signup-success', 1);
+	});
+}
+
