23839a0d029cc958003906100d5d737edb9438ef
Ryan Beck <ryan@bextechnologies.com>
2016-10-16 16:43:34 -0700

    new submit

--- assets/js/sendForm.js
+++ assets/js/sendForm.js
@@ -1,28 +1,38 @@
-
-$('#formcontactus').submit(function (event) {
-	event.preventDefault();
+$(document).ready(function() {
 	var URL = '<api-gateway-stage-url>/contact';
 	$console.log('hello');
- 
-	var data = {
-		name: $('#form-name').val(),
-		email: $('#form-email').val(),
-		prayer: $('#form-prayer').val()
-	}
- 
-	$.ajax({
-		type: 'POST',
-		url: URL,
-		dataType: 'json',
-		contentType: 'application/json',
-		data: JSON.stringify(data),
-		success: function () {
-			alert('Thank you for your feedback.');
-			document.getElementById("contactForm").reset();
-		},
-		error: function () {
-			alert('Your prayer has been submitted.');
-		 	document.getElementById("contactForm").reset();
+
+	$('#formcontactus').submit(function (event) {
+		$console.log('hello2');
+		event.preventDefault();
+    	ga('pray.send', 'event', 'buttons', 'click', 'prayer-submitted', 1);
+		ga('bex.send', 'event', 'buttons', 'click', 'prayer-submitted', 1);
+		var data = {
+			name: $('#form-name').val(),
+			email: $('#form-email').val(),
+			prayer: $('#form-prayer').val()
 		}
+
 	});
+
+	function sendEmail(data) {
+		$.ajax({
+			type: 'POST',
+			url: URL,
+			dataType: 'json',
+			contentType: 'application/json',
+			data: JSON.stringify(data),
+			success: function () {
+				alert('Thank you for your feedback.');
+				ga('pray.send', 'event', 'buttons', 'click', 'prayer-sent', 1);
+				ga('bex.send', 'event', 'buttons', 'click', 'prayer-sent', 1);
+				document.getElementById("contactForm").reset();
+			},
+			error: function () {
+				alert('Your prayer has been submitted.');
+			 	document.getElementById("contactForm").reset();
+			}
+		});
+
+	}
 });
